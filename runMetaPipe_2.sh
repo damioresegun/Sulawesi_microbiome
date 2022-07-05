c44f960d8983777a6aedf5382bfc2ce427cdaae1
Damilola Oresegun <damioresegun@gmail.com>
2022-07-05 01:19:35 +0100

    Lost my old file. Hope this fixes it

--- runMetaPipe_2.sh
+++ runMetaPipe_2.sh
@@ -2,30 +2,32 @@
 # Bash script to call the NanoMetaPipe python script
 #
 # enter variable
-NanoMetaPipe=/home/doresegu/scratch/private/JCS_MetaGenome_Project/NewNanoMetaPipe.py
+NanoMetaPipe=/home/doresegu/scratch/private/JCS_MetaGenome_Project/NanoMetaPipe.py
 reference=/home/doresegu/scratch/private/JCS_MetaGenome_Project/Index/Macaca_nemestrina_reference.fna.gz
 gff=/home/doresegu/scratch/private/JCS_MetaGenome_Project/Index/Macaca_nemestrina_genomic.gff.gz
-basecalled=/home/doresegu/scratch/private/JCS_MetaGenome_Project/MFMRCFS1622_BiM004/Basecalled/pass
-barcodes="barcode07 barcode08"
-isolates="MFMRCFS1622_DNA MFMRCFS1622_dscDNA"
-output=/home/doresegu/scratch/private/JCS_MetaGenome_Project/MFMRCFS1622_BiM004
-threads=12
+basecalled=/home/doresegu/scratch/private/JCS_MetaGenome_Project/MFMRCFS0722_WuM006/Basecalled/pass
+barcodes="barcode01 barcode02"
+isolates="MFMRCFS0722_DNA MFMRCFS0722_dscDNA"
+output=/home/doresegu/scratch/private/JCS_MetaGenome_Project/NewOutputs/TestingAgain
+threads=24
 demultiplexer="qcat"
 cdna_reads="/home/doresegu/scratch/private/JCS_MetaGenome_Project/Index/SRR10248516_1.fastq
             /home/doresegu/scratch/private/JCS_MetaGenome_Project/Index/SRR10248516_2.fastq"
 krakenDB=/home/doresegu/scratch/private/Kraken_DB
 cdna_adapters=/home/doresegu/scratch/private/JCS_MetaGenome_Project/Index/TruSeq3.fa
 cref=/home/doresegu/scratch/private/JCS_MetaGenome_Project/CDNA/Assembly/trinity_out/Trinity-GG.fasta # path to already made transcriptome
-maxMem="128G" # maximum memory
+maxMem="150G" # maximum memory
 kraken="kraken2"
 sequence_type="both"
 DnaFilt=500
 CdnaFilt=100
+bracken="bracken"
 
 
-
+mkdir $output
+touch ${output}/logfile.txt
 # both command and transcriptome path command
-run="python3 $NanoMetaPipe -b $basecalled -c $barcodes -e $isolates -r $reference -o $output -kb $krakenDB -kr $kraken -w -d $demultiplexer -dfl $DnaFilt -cfl $CdnaFilt -t $threads -g $gff -s $sequence_type -cr $cref -mxm $maxMem 2>&1 | tee ${output}/logfile.txt"
+run="python3 $NanoMetaPipe -b $basecalled -c $barcodes -e $isolates -r $reference -o $output -kb $krakenDB -kr $kraken -w -d $demultiplexer -dfl $DnaFilt -cfl $CdnaFilt -t $threads -g $gff -s $sequence_type -cr $cref -mxm $maxMem -br $bracken 2>&1 | tee ${output}/logfile.txt"
 
 # cdna command and transcriptome path command
 #run="python3 $NanoMetaPipe -b $basecalled -c $barcodes -e $isolates -r $reference -o $output -kb $krakenDB -kr $kraken -w -d $demultiplexer -t $threads -g $gff -s $sequence_type -cr $cref -mxm $maxMem 2>&1 | tee ${output}/logfile.txt"
